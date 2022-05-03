24f2f4c4cdfe91c24409af668707d2f805332e6b
Sardor Bayramov <BayramovSardor@gmail.com>
2022-05-04 04:42:16 +0500

    initial commit

---
+++ server.js
@@ -0,0 +1,81 @@
+const express = require('express');
+const crypto = require('crypto');
+const { promisify } = require('util');
+
+const app = express();
+const port = 4100;
+const scrypt = promisify(crypto.scrypt);
+
+app.set('view-engine', 'ejs');
+app.use(express.urlencoded({ extended: false }));
+
+let users = [];
+
+
+
+// main dashboard
+app.get('/', (req, res) => {
+  res.render('index.ejs', { users: JSON.stringify(users, false, 4) });
+});
+
+
+
+// Registration
+app.get('/register', (req, res) => {
+  res.render('register.ejs');
+});
+
+app.post('/register', async (req, res) => {
+  let salt = crypto.randomBytes(16).toString('hex');
+
+  try {
+    var passwordHash = await scrypt(req.body.password, salt, 64);
+  } catch (err) {
+    console.error(err);
+    res.redirect('/register');
+  }
+  
+  users.push({
+    id: Date.now().toString(),
+    name: req.body.name,
+    email: req.body.email,
+    password: salt + ':' + passwordHash.toString('hex')
+  });
+
+  console.log(users);
+  res.redirect('/');
+});
+
+
+
+// Logging in
+app.get('/login', (req, res) => {
+  res.render('login.ejs');
+});
+
+app.post('/login', async (req, res) => {
+  const user = users.find(user => user.email === req.body.email);
+  if (user == null) {
+    console.log('user email not found, email:', req.body.email);
+    return res.redirect('/login');
+  }
+  let [salt, oldPasswordHash] = user.password.split(':');
+  oldPasswordHash = Buffer.from(oldPasswordHash, 'hex');
+  try {
+    var passwordHash = await scrypt(req.body.password, salt, 64);
+  } catch (err) {
+    console.error(err);
+    res.redirect('/login');
+  }
+
+  if (crypto.timingSafeEqual(oldPasswordHash, passwordHash)) {
+    console.log(users);
+    res.redirect('/');
+  } else {
+    console.log('wrong password');
+    res.redirect('/login');
+  }
+});
+
+// Server port
+app.listen(port, () => console.log('Server listening on port:', port));
