19612652553549466283d43c31618c8f8813bab1
Sardor Bayramov <BayramovSardor@gmail.com>
2022-05-04 18:08:16 +0500

    p

--- server.js
+++ server.js
@@ -11,45 +11,40 @@ app.use(express.urlencoded({ extended: false }));
 
 let users = [];
 
-
-
-// main dashboard
+////////////////////////////////////////////////// Dashboard
 app.get('/', (req, res) => {
-  res.render('index.ejs', { users: JSON.stringify(users, false, 4) });
+  res.render('index.ejs', {
+    users: JSON.stringify(users, false, 4),
+    cookies: JSON.stringify(req.headers.cookie, false, 4)
+  });
 });
 
-
-
-// Registration
+////////////////////////////////////////////////// Registration
 app.get('/register', (req, res) => {
   res.render('register.ejs');
 });
 
 app.post('/register', async (req, res) => {
   let salt = crypto.randomBytes(16).toString('hex');
-
   try {
     var passwordHash = await scrypt(req.body.password, salt, 64);
   } catch (err) {
     console.error(err);
     res.redirect('/register');
   }
-  
   users.push({
     id: Date.now().toString(),
     name: req.body.name,
     email: req.body.email,
     password: salt + ':' + passwordHash.toString('hex')
   });
-
   console.log(users);
   res.redirect('/');
 });
 
-
-
-// Logging in
+////////////////////////////////////////////////// Logging in
 app.get('/login', (req, res) => {
+  res.setHeader('set-cookie', 'name=Sardor Bayramov'); // TODO
   res.render('login.ejs');
 });
 
@@ -67,15 +62,15 @@ app.post('/login', async (req, res) => {
     console.error(err);
     res.redirect('/login');
   }
-
   if (crypto.timingSafeEqual(oldPasswordHash, passwordHash)) {
     console.log(users);
     res.redirect('/');
   } else {
     console.log('wrong password');
+    res.setHeader('set-cookie', 'a=1');
     res.redirect('/login');
   }
 });
 
-// Server port
+////////////////////////////////////////////////// Application start
 app.listen(port, () => console.log('Server listening on port:', port));
